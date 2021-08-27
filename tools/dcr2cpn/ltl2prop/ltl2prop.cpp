a71a7f8875a1c8cd54f26f49719db4554647f8fa
Narutobacoshiba <haphapbk29@gmail.com>
2021-08-27 23:00:27 +0700

    update option for ltl2prop file

--- tools/dcr2cpn/ltl2prop/ltl2prop.cpp
+++ tools/dcr2cpn/ltl2prop/ltl2prop.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <map>
 #include <vector>
+#include "../CLI11.hpp"
 
 using namespace std;
 
@@ -50,7 +51,7 @@ string converLTL2LNAProp(string str){
     int strLength = str.length();
     for(int i = 0; i < strLength; i++){
         if(isSpecAChar(str[i]) && (str[i+1] == ' ' || str[i+1] == '(')){
-            ret.push_back(specAMap[str[i]]);
+            ret.push_back(specAMap[str[i]]+' ');
         }else if(isSpecNAChar(str[i])){
             ret.push_back(specNAMap[str[i]]+' ');
             if(temp.length() > 0){
@@ -58,7 +59,7 @@ string converLTL2LNAProp(string str){
                 temp = "";
             }
         }else if(str[i] == ' ' || str[i] == ')' || str[i] == '(' || i == strLength){
-            ret.push_back(temp+str[i]);
+            ret.push_back(temp+str[i]+' ');
             temp = "";
         }else{
             temp = temp + str[i];
@@ -69,12 +70,32 @@ string converLTL2LNAProp(string str){
     for(auto it = ret.begin(); it != ret.end(); it++){
         s << *it;
     }
-    return s.str());
+    return s.str();
 }
 
-int main(){
-    string test = "G(F  (uchiha ^  !   bedia))U((!ulata))";
-    string stringTokens = converLTL2LNAProp(test);
-    cout << stringTokens <<"\n";
+int main(int argc, char** argv){
+    CLI::App app{"Solidity2CPN tool"}; 
+    string LTL_FILE;
+    string OUT_FILE_NAME = "";
+    app.add_option("--ltl", LTL_FILE, "LTL File")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("--out_file", OUT_FILE_NAME, "Ouput File");
+    CLI11_PARSE(app, argc, argv); 
+
+    ifstream t(LTL_FILE);
+    stringstream buffer;
+    buffer << t.rdbuf();
+
+    string stringTokens = converLTL2LNAProp(buffer.str());
+    ofstream myfile;
+    if(OUT_FILE_NAME.compare("") == 0){
+        myfile.open (LTL_FILE.substr(0, LTL_FILE.find('.'))+".prop.lna");
+    }else{
+        myfile.open (OUT_FILE_NAME+".prop.lna");
+    }
+    myfile << "ltl property prop:\n\t";
+    myfile << stringTokens;
+    myfile.close();
     return 0;
 }
