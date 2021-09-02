cdc5669f8bd93905bdd6f1d6118a059066ed2314
Ngoc Anh <anhntnhe151378@fpt.edu.vn>
2021-09-02 17:05:58 +0700

    fix vul config

--- front-end/src/store/modules/data.js
+++ front-end/src/store/modules/data.js
@@ -7,7 +7,7 @@ const state = {
         selectedSCInfor: {},
         selectedContext: [],
         selectedVulnerbility: [],
-        configVul: {id: 1}
+        configVul: {}
     },
     views: {
         process: 'sc-selection',
@@ -94,18 +94,20 @@ const state = {
         var name = list_funcs[i].name
         list_lvs[name] = lvs
       }
-      
-      if(vuls.length > 0){
-        var obj = {}
-        if(Object.keys(state.data.selectedSCInfor[sc_id]).length > 0) obj = state.data.selectedSCInfor[sc_id];
-        var info = {list_gvs: list_gvs, list_lvs: list_lvs}
-        for(let i = 0; i< vuls.length; i++){
-          var vid = vuls[i].id
-          if(!(vid in obj) || Object.keys(obj[vid]).length === 0){
-            obj[vid] = info
+      if(sc_id in state.data.selectedSCInfor){
+        if(vuls.length > 0){
+          var obj = {}
+          if(state.data.selectedSCInfor[sc_id]) obj = state.data.selectedSCInfor[sc_id];
+          var info = {list_gvs: list_gvs, list_lvs: list_lvs}
+          for(let i = 0; i< vuls.length; i++){
+            var vid = vuls[i].id
+              if(!(vid in obj)) obj[vid] = info;
+          
+          }
+          state.data.selectedSCInfor[sc_id] = obj
+        }else{
+          state.data.selectedSCInfor[sc_id] = {}
         }
-        }
-        state.data.selectedSCInfor[sc_id] = obj
       }else{
         state.data.selectedSCInfor[sc_id] = {}
       }
@@ -115,6 +117,7 @@ const state = {
       delete state.data.selectedSCInfor[expriedkey];
     },
     addGVSInfor(state, {vul_id, sc_id, data}){
+      console.log(state.data.selectedSCInfor[sc_id][vul_id].list_gvs)
       state.data.selectedSCInfor[sc_id][vul_id].list_gvs = data
     },
     addLVSInfor(state, {sc_id,vul_id, func_name, data}){
