d11776c1be77348671c6c307a943ff8d35174fc9
leducanh11072000 <85017146+leducanh11072000@users.noreply.github.com>
2021-09-22 16:22:22 +0700

    Add files via upload

---
+++ createDatabasefulldata/14changeTableName.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user="root",
+    passwd = '',
+    database="Project"
+)
+mycursor = db.cursor()
+mycursor.excute("""ALTER TABLE old_table_name RENAME SmartContract;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Account;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Contact;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Functionss;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME LocalVariable;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Argument;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME GlobalVariable;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Context;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME SC_Context;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME Template;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME ProVul;""")
+mycursor.excute("""ALTER TABLE old_table_name RENAME SC_ProVul;""")
