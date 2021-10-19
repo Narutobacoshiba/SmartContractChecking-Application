b27c2a6ce7d442bcca5ac11f691cfc6ae05474d4
leducanh11072000 <85017146+leducanh11072000@users.noreply.github.com>
2021-10-19 23:23:25 +0700

    change design database

--- scripts/17-createCheckedSmartContractDetaildata.py
+++ scripts/17-createCheckedSmartContractDetaildata.py
@@ -10,12 +10,11 @@ def InsertIMG(FilePath):
         BinaryData = File.read()
     return BinaryData
 mycursor = db.cursor()
-sqlFomular = "INSERT INTO CheckedSmartContractDetail (status,isLTLTemplate,LTLFormula,result,lnid,sid,lteid,cid,bid,imid) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sqlFomular = "INSERT INTO CheckedSmartContractDetail (sid,bid) VALUES (%s,%s)"
 multi = [
-    (True,True,"LTLFOMULA","",1,1,1,1,1,1),
-    (True,False,"","244 places",2,2,2,2,2,2),
-    (False,True,"LTLFORMULA","",3,1,1,1,1,1),
-    (False,False,"","244 places /n 72 arcs",3,3,3,3,3,3)
+    (1,1),
+    (2,2),
+    (3,3),
     ]
 mycursor.executemany(sqlFomular,multi)
 db.commit()
