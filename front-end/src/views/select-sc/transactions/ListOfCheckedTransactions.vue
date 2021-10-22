cf1147cb3ecdcfa8a1eb114159a5d25f195a01d5
cityhunter2468 <85825064+cityhunter2468@users.noreply.github.com>
2021-10-22 17:42:12 +0700

    oke 1

--- front-end/src/views/select-sc/transactions/ListOfCheckedTransactions.vue
+++ front-end/src/views/select-sc/transactions/ListOfCheckedTransactions.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="main">
     <div id="header">
-    <h1>List of Checked Transactions</h1>
-    </div>
-
-    <div class="grey">
-      <span>Checked information</span>
+      <h1>Checked Smart Contract List</h1>
     </div>
 
     <div class="blue">
@@ -14,21 +10,26 @@
           <thead class="table-inside">
             <tr>
               <th style="width: 10%" scope="col">#</th>
-              <th style="width: 40%" scope="col">Batch Name</th>
-              <th style="width: 25%" scope="col">Checked Date</th>
-              <th style="width: 25%" scope="col">Description</th>
+              <th style="width: 20%" scope="col">Checker</th>
+              <th style="width: 30%" scope="col">Checked Date</th>
+              <th style="width: 40%" scope="col">Number of smart contracts</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(item, index) in getListTransaction" v-bind:key="index">
               <th scope="row">{{ index + 1 }}</th>
               <td>
-                <div v-on:click="set(item.bid)" v-bind:id="item.bid"><router-link
-                  :to="{path:'checkreentrancydetail', query: { id: item.bid }}"
-                  tag="a"
-                  class="lk"
-                  >{{ item.name }}</router-link
-                ></div>
+                <div v-on:click="set(item.bid)" v-bind:id="item.bid">
+                  <router-link
+                    :to="{
+                      path: 'checkreentrancydetail',
+                      query: { id: item.bid },
+                    }"
+                    tag="a"
+                    class="lk"
+                    >{{ item.name }}</router-link
+                  >
+                </div>
               </td>
               <td>{{ item.checkeddate }}</td>
               <td>{{ item.description }}</td>
@@ -38,9 +39,8 @@
       </div>
     </div>
     <div id="action">
-      <div id="btn-addsc" @click="routing('addsc')">Add Smart Contracts</div>
+      <div id="btn-addsc" @click="routing('addsc')">Smart a new checking session</div>
       <div id="btn-backnext" @click="routing('back')">Back</div>
-      <div id="btn-backnext" @click="routing('next')">Next</div>
     </div>
   </div>
 </template>
@@ -55,7 +55,6 @@ export default {
     };
   },
   methods: {
-    
     routing(param) {
       if (param == "next") {
         this.$router.push({ name: "CheckRentrancy" });
@@ -67,20 +66,19 @@ export default {
         this.$router.push({ name: "Index" });
       }
     },
-    ...mapActions(["getListTran","setid"]),
-    set(data){
+    ...mapActions(["getListTran", "setid"]),
+    set(data) {
       console.log(data);
-       this.setid(data);
-     }
+      this.setid(data);
+    },
   },
 
   computed: {
-    ...mapGetters(["getListTransaction","getid"]),
+    ...mapGetters(["getListTransaction", "getid"]),
   },
 
   created() {
     this.getListTran();
-    
   },
 };
 </script>
@@ -98,6 +96,7 @@ export default {
   background-color: #d9edf7;
   color: #3a7694;
 }
+
 .blue {
   border-radius: 10px;
   width: 80%;
@@ -106,24 +105,10 @@ export default {
   margin-right: 30%;
   height: 470px;
   color: black;
-}
-.grey {
-  width: 150px;
-  height: 30px;
-  color: black;
-}
-.grey {
-  margin-left: 11%;
-  margin-bottom: 25px;
-  background: white;
-  z-index: 3;
-  position: relative;
-}
-.blue {
   border: 1px solid #d9edf7;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-  margin-top: -40px;
+  margin-top: 40px;
   background: none;
   z-index: 2;
   position: relative;
