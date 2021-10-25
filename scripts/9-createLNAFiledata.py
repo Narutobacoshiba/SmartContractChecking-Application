a2fabdf18a5c8a01444363bece63fe2a85ce5877
cityhunter2468 <85825064+cityhunter2468@users.noreply.github.com>
2021-10-25 15:29:14 +0700

    oke

--- scripts/9-createLNAFiledata.py
+++ scripts/9-createLNAFiledata.py
@@ -16,14 +16,14 @@ def InsertIMG(FilePath):
 mycursor = db.cursor()
 sqlFomular = "INSERT INTO LNAFile (hcpnfile,propfile) VALUES (%s,%s)"
 multi = [
-    (InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
-     InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
-    (InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
-     InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
-    (InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
-     InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
-    (InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
-     InsertIMG(r"C:\Users\84984\Documents\GitHub\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
+    (InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
+     InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
+    (InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
+     InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
+    (InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
+     InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
+    (InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.lna"),
+     InsertIMG(r"D:\SmartContract\SmartContractChecking-Application\scripts\XMLfile\finnal_model.prop.lna")),
 
 
 ]
